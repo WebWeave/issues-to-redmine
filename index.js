603cb48f0b73d606dc03f98254b83f7919b35d32
Sander de Jonge <s.de.jonge@x-interactive.nl>
2019-04-25 17:03:01 +0200

    working demo

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
  * This is the main entrypoint
  * @param {import('probot').Application} app
  */
+
+const redmine = require('./lib/redmine')
+
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
   app.on('issues.opened', async context => {
+    // Insert issue into redmine
+    redmine.insertIssueFromGithub(context)
+
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     return context.github.issues.createComment(issueComment)
   })
